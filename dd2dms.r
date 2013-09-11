ac120a9c0ce8b18ae2f8b4d01329efb6f05741fe
weiminj <weimin.jiang@cawthron.org.nz>
2013-09-11 15:24:12 +1200

    a function to convert latlons from decimal degree to degree minute and seconds

--- dd2dms.r
+++ dd2dms.r
@@ -1,6 +1,20 @@
 dd2dms=function(x) {
   # a funciton to convert latlons from decimal degree to degree minute and seconds
+  #
+  # Sep 11, 2013, Weimin Jiang
+  #
   library(stringr)
+  if(is.null(dim(x))){
+    dot<-str_locate(x,pattern="\\.")[,1]
+    dd<-as.numeric(substr(x,1,dot-1))
+    m<-as.numeric(substr(x,dot,nchar(x)))*60
+    msc<-as.character(m)
+    dot.m<-str_locate(msc,pattern="\\.")[,1]
+    mm<-trunc(m)
+    mm<-ifelse(mm<10,paste("0",mm,sep=""),mm)
+    ss<-as.numeric(substr(msc,dot.m,nchar(msc)))*60
+    out<-paste(dd,mm,ss,sep=" ")
+  } else {
   latlon<-apply(x,2,as.character)
   out<-apply(latlon,2,FUN=function(x){
   dot<-str_locate(x,pattern="\\.")[,1]
@@ -12,6 +26,6 @@ dd2dms=function(x) {
   mm<-ifelse(mm<10,paste("0",mm,sep=""),mm)
   ss<-as.numeric(substr(msc,dot.m,nchar(msc)))*60
   paste(dd,mm,ss,sep=" ")
-})
+})}
   out
 }
